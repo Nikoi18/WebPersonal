b73b8e0460a2bef2cf020b08fca5eda65b5c792a
Nicolas A. Urbaez A. <aran.nick15@gmail.com>
2025-05-25 21:50:04 +0200

    WebPersonal Finalizada

--- WebPersonal/WebPersonal/urls.py
+++ WebPersonal/WebPersonal/urls.py
@@ -2,16 +2,21 @@
 
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core import views as core
+from django.conf import settings
+from portfolio import views as port
+
 
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about-me/', views.about, name='about'),
-    path('portfolio/', views.portfolio, name='portfolio'),
-    path('contact/', views.contact, name='contact'),
-    path('admin/', admin.site.urls),
-    
-    
+    path('', core.home, name='home'),
+    path('about-me/', core.about, name='about'),
+    path('portfolio/', port.portfolio, name='portfolio'),
+    path('contact/', core.contact, name='contact'),
+    path('admin/', admin.site.urls),  
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
